2be496e5ee067ee67f8a63926a1f14cd04042cb9
Kyle Eriss <kyle.eriss@gmail.com>
2021-02-10 18:12:38 -0500

    additional config changes

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
+    API_BASE_URL: process.env.REACT_APP_API_BASE_URL || "http://localhost:3000/api",
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: process.env.DB_URL || 'postgresql://postgres@localhost/reconnaissound',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/reconnaissound',
     JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
 }
